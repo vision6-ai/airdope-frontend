3d922a6e1d8da462b77d50bff175deb03b521619
vision6-ai <yinon@vision6.ai>
2025-12-27 22:08:07 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/",
   css: {
     postcss: {
       plugins: [tailwind()],
